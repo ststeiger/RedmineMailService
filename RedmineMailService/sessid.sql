b7cb7f0a28a26c2c5d8040d055a24a765452c71a
ststeiger <quandary82@hailmail.net>
2018-05-18 17:36:47 +0200

    multicheck

---
+++ RedmineMailService/sessid.sql
@@ -0,0 +1,5 @@
+
+select lower(replace(newid(), '-', '')), len(replace(newid(), '-', '')), len('c791d33f1a734a3782064bc719682fed')
+http://localhost/ReportServer_SQLExpress
+
+http://COR-W10-112/ReportServer_SQLEXPRESS
