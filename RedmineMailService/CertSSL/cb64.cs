4c05e13d74cc50633c6c3993c2e800da035ebdd4
ststeiger <quandary82@hailmail.net>
2018-10-31 17:06:12 +0100

    Base64 CertUtil

---
+++ RedmineMailService/CertSSL/cb64.cs
@@ -0,0 +1,104 @@
+﻿
+namespace RedmineMailService.CertSSL
+{
+
+
+    public class cb64
+    {
+        private const int base64LineBreakPosition = 64;
+
+        internal static readonly char[] base64Table = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+                                                       'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
+                                                       'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                                                       't','u','v','w','x','y','z','0','1','2','3','4','5','6','7',
+                                                       '8','9','+','/','=' };
+
+
+
+        // public static int ConvertToBase64Array(System.IO.StreamWriter sw, byte[] inData, int offset, int length, int totalLength, bool insertLineBreaks)
+        // https://www.codeproject.com/Articles/5483/Base-Encoder-Decoder-in-C
+        // https://github.com/mono/mono/blob/master/mcs/class/referencesource/mscorlib/system/convert.cs
+        public static int ConvertToBase64Array(System.IO.StreamWriter sw, byte[] inData, int offset, int length, bool insertLineBreaks, bool isFinal)
+        {
+            int lengthmod3 = length % 3;
+            int calcLength = offset + (length - lengthmod3);
+            int j = 0;
+            int charcount = 0;
+            //Convert three bytes at a time to base64 notation.  This will consume 4 chars.
+            int i;
+
+            // get a pointer to the base64Table to avoid unnecessary range checking
+            {
+                for (i = offset; i < calcLength; i += 3)
+                {
+                    if (insertLineBreaks)
+                    {
+                        if (charcount == base64LineBreakPosition)
+                        {
+                            sw.Write("\r\n");
+                            j += 2;
+                            charcount = 0;
+                        }
+                        charcount += 4;
+                    }
+                    sw.Write(base64Table[(inData[i] & 0xfc) >> 2]);
+                    sw.Write(base64Table[((inData[i] & 0x03) << 4) | ((inData[i + 1] & 0xf0) >> 4)]);
+                    sw.Write(base64Table[((inData[i + 1] & 0x0f) << 2) | ((inData[i + 2] & 0xc0) >> 6)]);
+                    sw.Write(base64Table[(inData[i + 2] & 0x3f)]);
+                    j += 4;
+                }
+
+                //Where we left off before
+                i = calcLength;
+
+                if (insertLineBreaks && (lengthmod3 != 0 || !isFinal) && (charcount == base64LineBreakPosition))
+                {
+                    sw.Write("\r\n");
+                    j += 2;
+                }
+
+                switch (lengthmod3)
+                {
+                    case 2: //One character padding needed
+                        // char char1 = base64Table[(inData[i] & 0xfc) >> 2];
+                        // char char2 = base64Table[((inData[i] & 0x03) << 4) | ((inData[i + 1] & 0xf0) >> 4)];
+                        // char char3 = base64Table[(inData[i + 1] & 0x0f) << 2];
+                        // char char4 = base64Table[64]; //Pad
+
+                        // string e = new string(new char[] { char1, char2, char3, char4 });
+                        // System.Console.WriteLine(e);
+
+
+                        sw.Write(base64Table[(inData[i] & 0xfc) >> 2]);
+                        sw.Write(base64Table[((inData[i] & 0x03) << 4) | ((inData[i + 1] & 0xf0) >> 4)]);
+                        sw.Write(base64Table[(inData[i + 1] & 0x0f) << 2]);
+                        sw.Write(base64Table[64]); //Pad
+                        j += 4;
+                        break;
+                    case 1: // Two character padding needed
+
+                        // char char11 = base64Table[(inData[i] & 0xfc) >> 2];
+                        // char char12 = base64Table[(inData[i] & 0x03) << 4];
+                        // char char13 = base64Table[64]; //Pad
+                        // char char14 = base64Table[64]; //Pad
+
+                        // string ee = new string(new char[] { char11, char12, char13, char14 });
+                        // System.Console.WriteLine(ee);
+
+                        sw.Write(base64Table[(inData[i] & 0xfc) >> 2]);
+                        sw.Write(base64Table[(inData[i] & 0x03) << 4]);
+                        sw.Write(base64Table[64]); //Pad
+                        sw.Write(base64Table[64]); //Pad
+                        j += 4;
+                        break;
+                }
+            }
+
+            return j;
+        }
+        
+
+    }
+
+
+}
--- RedmineMailService/Program.cs
+++ RedmineMailService/Program.cs
@@ -81,12 +81,74 @@ namespace RedmineMailService
         }
 
 
+        public static void ToCertUtil()
+        {
+            string inputFile = @"D:\username\Desktop\RSS\Desktop.zip";
+            string outputFile = @"D:\username\Desktop\RSS\Desktop_plain.txt";
+
+            //const int BUFFER_SIZE = 4032; // 4032%3=0 && 4032%64=0
+            const int BUFFER_SIZE = 4080; // 4080%3=0 && 4080%8=0
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            using (System.IO.FileStream outputStream = System.IO.File.OpenWrite(outputFile))
+            {
+                
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(outputStream, System.Text.Encoding.ASCII))
+                {
+                    sw.Write("-----BEGIN CERTIFICATE-----");
+                    sw.Write(System.Environment.NewLine);
+
+                    using (System.IO.FileStream inputStream = System.IO.File.OpenRead(inputFile))
+                    {
+                        using (System.IO.BinaryReader br = new System.IO.BinaryReader(inputStream))
+                        {
+                            br.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                            long totalLength = inputStream.Length;
+
+
+                            long totalRead = 0;
+                            int bytesRead;
+                            while ((bytesRead = br.Read(buffer, 0, BUFFER_SIZE)) > 0)
+                            {
+                                totalRead += bytesRead;
+
+                                // string b64 = System.Convert.ToBase64String(buffer, 0, bytesRead, System.Base64FormattingOptions.InsertLineBreaks);
+                                // string b64 = System.Convert.ToBase64String(buffer);
+
+                                bool isFinal = (bytesRead < BUFFER_SIZE || totalRead == totalLength);
+                                CertSSL.cb64.ConvertToBase64Array(sw, buffer, 0, bytesRead, true, isFinal);
+
+                                //if (bytesRead < BUFFER_SIZE || totalRead == totalLength)
+                                //    b64 = b64.Substring(0, b64.Length - 2);
+
+                                //sw.Write(b64);
+
+                                // sw.Write(System.Environment.NewLine);
+                                // CertSSL.cb64.ConvertToBase64Array(sw, buffer, 0, bytesRead, true);
+                            } // Whend 
+
+                            br.Close();
+                        } // End Using br 
+
+                    } // End Using inputStream 
+
+                    sw.Write(System.Environment.NewLine);
+                    sw.Write("-----END CERTIFICATE-----");
+                    sw.Write(System.Environment.NewLine);
+                } // End Using sw 
+
+            } // End Using outputStream 
+
+        } // End Sub ToCertUtil 
+
 
         // https://twitter.com/gitlost
         // https://badhtml.com/
         [System.STAThread]
         static void Main(string[] args)
         {
+            ToCertUtil();
+
             // AnySqlWebAdmin.CerGenerator.CreateSignatureRequest();
             AnySqlWebAdmin.CerGenerator.Test();
             // RedmineMailService.MailGender.GetGenders();
