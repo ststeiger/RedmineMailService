9a04ea75f5f0cdec234c0d8aac05fe594dbf6c78
ststeiger <quandary82@hailmail.net>
2018-10-31 10:04:24 +0100

    Fix Certificate installer

--- RedmineMailService/CertSSL/Certificator.cs
+++ RedmineMailService/CertSSL/Certificator.cs
@@ -1,19 +1,166 @@
 
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
 
 
+// https://gist.github.com/ststeiger/aa7da5031e689fd66527af86ad962cc0
+// https://dotnetcodr.com/2016/01/21/using-client-certificates-in-net-part-4-working-with-client-certificates-in-code/
 namespace RedmineMailService.CertSSL
 {
+
+
     public class Certificator
     {
+
+        /// <summary>
+        ///     Ensure certificates are setup (creates root if required).
+        ///     Also makes root certificate trusted based on initial setup from proxy constructor for user/machine trust.
+        /// </summary>
+        public void EnsureRootCertificate()
+        {
+            X509Certificate2 rootCertificate = null;
+            bool machineTrustRoot = false;
+
+            if (rootCertificate == null)
+            {
+                rootCertificate = CreateRootCertificate();
+            }
+
+            TrustRootCertificate(rootCertificate, machineTrustRoot);
+        }
+
+        public static X509Certificate2 CreateRootCertificate()
+        {
+            return null;
+        }
+
+
+        /// <summary>
+        ///     Trusts the root certificate in user store, optionally also in machine store.
+        ///     Machine trust would require elevated permissions (will silently fail otherwise).
+        /// </summary>
+        public void TrustRootCertificate(X509Certificate2 cert, bool machineTrusted = false)
+        {
+            // currentUser\personal
+            InstallCertificate(cert, StoreName.My, StoreLocation.CurrentUser);
+
+            if (!machineTrusted)
+            {
+                // currentUser\Root
+                InstallCertificate(cert, StoreName.Root, StoreLocation.CurrentUser);
+            }
+            else
+            {
+                // current system
+                InstallCertificate(cert, StoreName.My, StoreLocation.LocalMachine);
+
+                // this adds to both currentUser\Root & currentMachine\Root
+                InstallCertificate(cert, StoreName.Root, StoreLocation.LocalMachine);
+            }
+        }
+
+
+        private static X509Certificate Find(string serialNumber, StoreLocation location)
+        {
+            using (X509Store store = new X509Store(location))
+            {
+                store.Open(OpenFlags.OpenExistingOnly);
+                X509Certificate2Collection certs = store.Certificates.Find(X509FindType.FindBySerialNumber, serialNumber, true);
+                return certs.OfType<X509Certificate>().FirstOrDefault();
+            }
+        }
+
+        public static bool certExists()
+        {
+            // TODO: 
+            // StoreLocation.CurrentUser)
+
+            using (X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection certificates = store.Certificates.Find(
+                    X509FindType.FindBySubjectName,
+                    "subjectName",
+                    false);
+
+                if (certificates != null && certificates.Count > 0)
+                {
+                    System.Console.WriteLine("Certificate already exists");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public static void PrintCertificateInfo(X509Certificate2 certificate)
+        {
+            Console.WriteLine("Name: {0}", certificate.FriendlyName);
+            Console.WriteLine("Issuer: {0}", certificate.IssuerName.Name);
+            Console.WriteLine("Subject: {0}", certificate.SubjectName.Name);
+            Console.WriteLine("Version: {0}", certificate.Version);
+            Console.WriteLine("Valid from: {0}", certificate.NotBefore);
+            Console.WriteLine("Valid until: {0}", certificate.NotAfter);
+            Console.WriteLine("Serial number: {0}", certificate.SerialNumber);
+            Console.WriteLine("Signature Algorithm: {0}", certificate.SignatureAlgorithm.FriendlyName);
+            Console.WriteLine("Thumbprint: {0}", certificate.Thumbprint);
+            Console.WriteLine();
+        }
+
+        public static void EnumCertificates(StoreName name, StoreLocation location)
+        {
+            using (X509Store store = new X509Store(name, location))
+            {
+                try
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    foreach (X509Certificate2 certificate in store.Certificates)
+                    {
+                        PrintCertificateInfo(certificate);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    store.Close();
+                }
+            }
+        }
+
+        public static void EnumCertificates(string name, StoreLocation location)
+        {
+            using (X509Store store = new X509Store(name, location))
+            {
+                try
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    foreach (X509Certificate2 certificate in store.Certificates)
+                    {
+                        PrintCertificateInfo(certificate);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    store.Close();
+                }
+            }
+        }
 
 
         // <summary>
@@ -26,29 +173,126 @@ namespace RedmineMailService.CertSSL
             //string issuer = $"{RootCertificate.Issuer}";
             // && (CertificateEngine != CertificateEngine.DefaultWindows
             return FindCertificates(StoreName.Root, storeLocation, issuer).Count > 0 
-                       || FindCertificates(StoreName.My, storeLocation, issuer).Count > 0);
-        }
-        
-        
+                       || FindCertificates(StoreName.My, storeLocation, issuer).Count > 0;
+        } // End Function FindCertificates 
+
+
         private static X509Certificate2Collection FindCertificates(
               StoreName storeName
             , StoreLocation storeLocation
             , string findValue)
         {
-            var x509Store = new X509Store(storeName, storeLocation);
+            X509Certificate2Collection results = null;
+
+            using (X509Store x509Store = new X509Store(storeName, storeLocation))
+            {
+                try
+                {
+                    x509Store.Open(OpenFlags.OpenExistingOnly);
+                    results = x509Store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, findValue, false);
+                }
+                finally
+                {
+                    x509Store.Close();
+                }
+
+            } // End Using x509Store 
+
+            return results;
+        } // End Function FindCertificates 
+
+        // https://www.codeguru.com/csharp/csharp/cs_misc/security/article.php/c16491/Working-with-Digital-Certificates-in-NET.htm
+        public static bool ExportCertificate(
+           string certificateName,
+           string path,
+           StoreName storeName,
+           StoreLocation location)
+        {
+            bool success = false;
+
+            X509Store store = new X509Store(storeName, location);
+            store.Open(OpenFlags.ReadOnly);
             try
             {
-                x509Store.Open(OpenFlags.OpenExistingOnly);
-                return x509Store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, findValue, false);
+                X509Certificate2Collection certs
+                   = store.Certificates.Find(X509FindType.FindBySubjectName, certificateName, true);
+
+                if (certs != null && certs.Count > 0)
+                {
+                    byte[] data = certs[0].Export(X509ContentType.Cert);
+                    success = WriteFile(data, path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             finally
             {
-                x509Store.Close();
+                store.Close();
             }
+
+            return success;
         }
 
-        
-        
+        public static bool ExportCertificate(
+           string certificateName,
+           string path,
+           string storeName,
+           StoreLocation location)
+        {
+            bool success = false;
+
+            using (X509Store store = new X509Store(storeName, location))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                try
+                {
+                    X509Certificate2Collection certs
+                       = store.Certificates.Find(X509FindType.FindBySubjectName, certificateName, true);
+
+                    if (certs != null && certs.Count > 0)
+                    {
+                        byte[] data = certs[0].Export(X509ContentType.Cert);
+                        success = WriteFile(data, path);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    store.Close();
+                }
+            }
+
+            return success;
+        }
+
+        private static bool WriteFile(byte[] data, string filename)
+        {
+            bool ret = false;
+            try
+            {
+                using (FileStream f = new FileStream(filename, FileMode.Create, FileAccess.Write))
+                {
+                    f.Write(data, 0, data.Length);
+                    f.Flush();
+                    f.Close();
+                }
+                
+                ret = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return ret;
+        }
+
+
 
         /// <summary>
         ///     Loads root certificate from current executing assembly location with expected name rootCert.pfx.
@@ -58,9 +302,9 @@ namespace RedmineMailService.CertSSL
             , string pfxPassword)
         {
             return LoadRootCertificate(fileName, pfxPassword, X509KeyStorageFlags.Exportable);
-        }
-        
-        
+        } // End Function LoadRootCertificate 
+
+
         /// <summary>
         ///     Loads root certificate from current executing assembly location with expected name rootCert.pfx.
         /// </summary>
@@ -83,84 +327,92 @@ namespace RedmineMailService.CertSSL
             {
                 throw e;
             }
-        }
+        } // End Function LoadRootCertificate 
 
-        
-        
+
+
         /// <summary>
         ///     Remove the Root Certificate trust
         /// </summary>
         /// <param name="storeName"></param>
         /// <param name="storeLocation"></param>
         /// <param name="certificate"></param>
-        public static void UninstallCertificate(StoreName storeName, StoreLocation storeLocation,
-            X509Certificate2 certificate)
+        public static void UninstallCertificate(
+              X509Certificate2 certificate
+            , StoreName storeName
+            , StoreLocation storeLocation
+            )
         {
             if (certificate == null)
             {
                 throw new Exception("Could not remove certificate as it is null or empty.");
             }
 
-            var x509Store = new X509Store(storeName, storeLocation);
-
-            try
+            using (X509Store x509Store = new X509Store(storeName, storeLocation))
             {
-                x509Store.Open(OpenFlags.ReadWrite);
+                try
+                {
+                    x509Store.Open(OpenFlags.ReadWrite);
+                    x509Store.Remove(certificate);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Failed to remove root certificate trust "
+                                      + $" for {storeLocation} store location. You may need admin rights.", e);
+                }
+                finally
+                {
+                    x509Store.Close();
+                }
+            } // End Using x509Store 
 
-                x509Store.Remove(certificate);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Failed to remove root certificate trust "
-                                  + $" for {storeLocation} store location. You may need admin rights.", e);
-            }
-            finally
-            {
-                x509Store.Close();
-            }
-        }
+        } // End Sub UninstallCertificate 
 
-        
-        
+
+
         /// <summary>
         ///     Make current machine trust the Root Certificate used by this proxy
         /// </summary>
         /// <param name="storeName"></param>
         /// <param name="storeLocation"></param>
         /// <param name="certificate"></param>
-        public static void InstallCertificate(StoreName storeName, StoreLocation storeLocation
-            ,System.Security.Cryptography.X509Certificates.X509Certificate2 certificate;
+        public static void InstallCertificate(
+              System.Security.Cryptography.X509Certificates.X509Certificate2 certificate
+            , StoreName storeName
+            , StoreLocation storeLocation
+            
         )
         {
             
             if (certificate == null)
             {
                 throw new Exception("Could not install certificate as it is null or empty.");
-                return;
             }
 
-            X509Store x509Store = new X509Store(storeName, storeLocation);
-            
-            // todo
-            // also it should do not duplicate if certificate already exists
-            try
+            using (X509Store x509Store = new X509Store(storeName, storeLocation))
             {
-                x509Store.Open(OpenFlags.ReadWrite);
-                x509Store.Add(certificate);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Failed to make system trust root certificate "
-                                  + $" for {storeName}\\{storeLocation} store location. You may need admin rights.",
-                        e);
-            }
-            finally
-            {
-                x509Store.Close();
-            }
-        }
-        
-        
+                // todo
+                // also it should do not duplicate if certificate already exists
+                try
+                {
+                    x509Store.Open(OpenFlags.ReadWrite);
+                    x509Store.Add(certificate);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Failed to make system trust root certificate "
+                                      + $" for {storeName}\\{storeLocation} store location. You may need admin rights.",
+                            e);
+                }
+                finally
+                {
+                    x509Store.Close();
+                }
+            } // End Using x509Store 
+
+        } // End Sub InstallCertificate 
+
+
     }
     
     
---
+++ RedmineMailService/CertSSL/NonBackdooredPrng.cs
@@ -0,0 +1,170 @@
+﻿
+namespace RedmineMailService.CertSSL
+{
+
+    public abstract class NonBackdooredPrng : Org.BouncyCastle.Crypto.Prng.IRandomGenerator
+    {
+        public abstract void AddSeedMaterial(byte[] seed);
+        public abstract void AddSeedMaterial(long seed);
+        public abstract void NextBytes(byte[] bytes);
+        public abstract void NextBytes(byte[] bytes, int start, int len);
+
+
+        public static NonBackdooredPrng Create()
+        {
+            bool isWindows =
+                System.Runtime.InteropServices.RuntimeInformation
+                    .IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
+
+            if (isWindows)
+                return new WindowsPrng();
+
+            return new PosixPrng();
+        } // End Function Create 
+
+
+        public static Org.BouncyCastle.Security.SecureRandom SecureRandom
+        {
+            get
+            {
+                return new Org.BouncyCastle.Security.SecureRandom(
+                    NonBackdooredPrng.Create()
+                );
+            }
+        }
+
+
+    } // End Class NonBackdooredPrng 
+
+
+
+    public class WindowsPrng : NonBackdooredPrng
+    {
+        protected Org.BouncyCastle.Crypto.Prng.IRandomGenerator m_rnd;
+
+        public WindowsPrng()
+        {
+            // this.m_rnd = new Org.BouncyCastle.Crypto.Prng.CryptoApiRandomGenerator();
+
+            const string digestName = "SHA256";
+            Org.BouncyCastle.Crypto.IDigest digest = Org.BouncyCastle.Security.DigestUtilities.GetDigest(digestName);
+            if (digest == null)
+                return;
+
+            Org.BouncyCastle.Crypto.Prng.DigestRandomGenerator prng =
+                new Org.BouncyCastle.Crypto.Prng.DigestRandomGenerator(digest);
+
+            const bool autoSeed = true;
+            if (autoSeed)
+            {
+                // prng.AddSeedMaterial(NextCounterValue());
+                // prng.AddSeedMaterial(GetNextBytes(Master, digest.GetDigestSize()));
+            }
+
+            this.m_rnd = prng;
+        }
+
+
+        /// <summary>Add more seed material to the generator.</summary>
+        /// <param name="seed">A byte array to be mixed into the generator's state.</param>
+        public override void AddSeedMaterial(byte[] seed)
+        {
+            this.m_rnd.AddSeedMaterial(seed);
+        } // End Sub AddSeedMaterial 
+
+
+        /// <summary>Add more seed material to the generator.</summary>
+        /// <param name="seed">A long value to be mixed into the generator's state.</param>
+        public override void AddSeedMaterial(long seed)
+        {
+            this.m_rnd.AddSeedMaterial(seed);
+        } // End Sub AddSeedMaterial 
+
+
+        /// <summary>Fill byte array with random values.</summary>
+        /// <param name="bytes">Array to be filled.</param>
+        public override void NextBytes(byte[] bytes)
+        {
+            this.m_rnd.NextBytes(bytes);
+        } // End Sub NextBytes 
+
+
+        /// <summary>Fill byte array with random values.</summary>
+        /// <param name="bytes">Array to receive bytes.</param>
+        /// <param name="start">Index to start filling at.</param>
+        /// <param name="len">Length of segment to fill.</param>
+        public override void NextBytes(byte[] bytes, int start, int len)
+        {
+            this.m_rnd.NextBytes(bytes, start, len);
+        } // End Sub NextBytes 
+
+    } // End Class WindowsPrng
+
+
+    public class PosixPrng : NonBackdooredPrng
+    {
+        // Early boot on a very low entropy device. 
+        // The /dev/urandom device cannot guarantee 
+        // that it has received enough initial entropy, 
+        // while when using /dev/random that is guaranteed 
+        // (even if it may block).
+        // therefore, use /dev/urandom
+
+        // /dev/random //  potentially blocking
+        // /dev/urandom
+
+
+        /// <summary>Add more seed material to the generator.</summary>
+        /// <param name="seed">A byte array to be mixed into the generator's state.</param>
+        public override void AddSeedMaterial(byte[] seed)
+        {
+            // throw new System.NotImplementedException();
+        } // End Sub AddSeedMaterial 
+
+
+        /// <summary>Add more seed material to the generator.</summary>
+        /// <param name="seed">A long value to be mixed into the generator's state.</param>
+        public override void AddSeedMaterial(long seed)
+        {
+            // throw new System.NotImplementedException();
+        } // End Sub AddSeedMaterial 
+
+
+        /// <summary>Fill byte array with random values.</summary>
+        /// <param name="bytes">Array to be filled.</param>
+        public override void NextBytes(byte[] bytes)
+        {
+            using (System.IO.FileStream fs =
+                new System.IO.FileStream(
+                  "/dev/urandom"
+                , System.IO.FileMode.Open
+                , System.IO.FileAccess.Read))
+            {
+                fs.Read(bytes, 0, bytes.Length);
+            }
+
+        } // End Sub NextBytes 
+
+
+        /// <summary>Fill byte array with random values.</summary>
+        /// <param name="bytes">Array to receive bytes.</param>
+        /// <param name="start">Index to start filling at.</param>
+        /// <param name="len">Length of segment to fill.</param>
+        public override void NextBytes(byte[] bytes, int start, int len)
+        {
+            using (System.IO.FileStream fs =
+                new System.IO.FileStream(
+                  "/dev/urandom"
+                , System.IO.FileMode.Open
+                , System.IO.FileAccess.Read))
+            {
+                fs.Read(bytes, start, len);
+            }
+
+        } // End Sub NextBytes 
+
+
+    } // End Class LinuxPrng 
+
+
+} // End Namespace CoreCMS.JWT 
